f0784161289b2d35d28bc6e388a27b60d2caa7af
kauamassei <gameplayultra11@gmail.com>
2025-02-15 02:07:10 -0300

    Ajustes para o deploy

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,22 +1,31 @@
 import express from "express"
 import { db } from "./connect.js";
 import cors from "cors";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express(); // chamando a função express pra variavel app
 const PORT = 3001 // declarando a porta
 
 app.use(cors())
 
-app.get("/", (request, response) => { // endpoint = rota
+app.get("/api/", (request, response) => { // endpoint = rota
     response.send("Olá, Mundo!") //response.send envia a mensagem 
 });
 
-app.get("/artists", async(request, response) => { // endpoint = rota
+app.get("/api/artists", async(request, response) => { // endpoint = rota
     response.send(await db.collection("artists").find({}).toArray())
 });
 
-app.get("/songs", async(request, response) => { // endpoint = rota
+app.get("/api/songs", async(request, response) => { // endpoint = rota
     response.send(await db.collection("songs").find({}).toArray())
+});
+
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+app.get("*", async(request, response) => {
+    response.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
 });
 
 app.listen(PORT, () => {
